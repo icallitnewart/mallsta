a536c879bb2fe443fbda6d0cfc1f37f29e48c094
icallitnewart <icallitnewart@gmail.com>
2022-06-09 11:45:17 +0900

    Design: Navbar UI구현

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -1,8 +1,73 @@
-import React from 'react';
-import { Header, Inner, Nav, Ul, Li } from "../../styles/NavbarStyle";
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { Header, Inner, Nav, Ul, Li, Dropdown,DropdownBox, DropdownItems } from "../../styles/NavbarStyle";
 import { Logo } from "../../styles/LogoStyle";
+import { BsSuitHeart, BsCart } from "react-icons/bs";
+import { IoPersonOutline } from "react-icons/io5";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const isLoggedIn = true;
+  const [ showDropdown, setShowDropdown ] = useState(false);
+
+  //계정 아이콘 클릭시
+  const handleAccountClick = (e)=> {
+    //로그인했을 경우
+    if(isLoggedIn) {
+      e.preventDefault();
+      setShowDropdown(!showDropdown);
+    }
+  };
+
+  //하트 아이콘 클릭시
+  const handleHeartClick = (e)=> {
+    //로그인 안 했을 경우
+    if(!isLoggedIn) {
+      e.preventDefault();
+      if(window.confirm("This feature requires login. Would you like to login?")) {
+        navigate("/login");
+      }
+    }
+  };
+
+  const renderNav = ()=> {
+    return (
+      <Ul>
+        <Li>
+          <Link to="/likes" onClick={handleHeartClick}>
+            <BsSuitHeart />
+          </Link>
+        </Li>
+        <Li isCartEmpty={false}>
+          <Link to="/cart">
+            <BsCart />
+          </Link>
+        </Li>
+        <Li>
+          <Link to="/login" onClick={handleAccountClick}>
+            <IoPersonOutline />
+          </Link>
+          {/* 계정 소메뉴 */}
+          {(isLoggedIn && showDropdown) &&
+          <Dropdown>
+            <DropdownBox>
+              <DropdownItems>
+                <Link to="/">My Page</Link>
+              </DropdownItems>
+              <DropdownItems>
+                <Link to="/">Settings</Link>
+              </DropdownItems>
+              <DropdownItems>
+                <Link to="/">Logout</Link>
+              </DropdownItems>
+            </DropdownBox>
+          </Dropdown>
+          }
+        </Li>
+      </Ul>
+    )
+  };
+  
   return (
     <Header>
       <Inner>
@@ -13,11 +78,7 @@ function Navbar() {
         >Mallsta</Logo>
 
         <Nav>
-          <Ul>
-            <Li></Li>
-            <Li></Li>
-            <Li></Li>
-          </Ul>
+          {renderNav()}
         </Nav>
       </Inner>
     </Header>
--- client/src/styles/NavbarStyle.js
+++ client/src/styles/NavbarStyle.js
@@ -25,8 +25,78 @@ export const Ul = styled.ul`
 `;
 
 export const Li = styled.li`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 30px;
   height: 30px;
-  background: peachpuff;
   margin-left: 15px;
+
+  svg {
+    font-size: 20px;
+    color: #ff5e62;
+    cursor: pointer;
+    padding: 5px;
+    box-sizing: content-box;
+  }
+
+  //장바구니 아이템 개수 아이콘
+  ${props=> (props.isCartEmpty===false) && css`
+    &::after {
+      position: absolute;
+      top: 2px; left: 15px;
+      display: block;
+      content: "100";
+      padding: 3px;
+      color: white;
+      font: 8px/9px "Poppins";
+      border-radius: 50%;
+      background: #f64146;
+    }`
+  }
+`;
+
+export const Dropdown = styled.div`
+  position: absolute;
+  top: 42px; right: -30px;
+  width: 160px;
+  background: #fff;
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+
+  &::before {
+    position: absolute;
+    top: -6px; right: 38px;
+    z-index: 1000;
+    display: inline-block;
+    content: "";
+    width: 12px;
+    height: 12px;
+    background: #fff;
+    transform: rotate(45deg);
+    box-shadow: -2px -2px 3px rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export const DropdownBox = styled.ul`
+  padding: 10px 0px;
+`;
+
+export const DropdownItems = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  cursor: pointer;
+  
+  a {
+    font: 13px/1 "Poppins";
+    color: #666;
+  }
+
+  &:hover a {
+    color: #ff5e62;
+  }
 `;
