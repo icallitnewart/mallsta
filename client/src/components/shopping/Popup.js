5ac616f90ecb003f6d72889f4f2b0ee5e23f7780
icallitnewart <icallitnewart@gmail.com>
2022-06-24 18:10:12 +0900

    Fix: 팝업창 생성시 스크롤 방지

--- client/src/components/shopping/Popup.js
+++ client/src/components/shopping/Popup.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Background, Container } from "../../styles/shopping/PopupStyle";
 
 import PostUpload from './PostUpload';
 
 function Popup({ username, productId, setIsUpload }) {
+  useEffect(()=> {
+    document.body.style.overflow = "hidden";
+
+    return ()=> {
+      document.body.style.overflow = "auto";
+    }
+  }, []);
 
   return (
     <Background>
