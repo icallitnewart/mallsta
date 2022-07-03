93c459a7a8cff9222aa032cde3e01672cabf2d77
icallitnewart <icallitnewart@gmail.com>
2022-07-04 04:55:01 +0900

    Chore: redux-persist 설치 및 셋팅 (auth userData에 적용)

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,12 +1,25 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { createFilter } from "redux-persist-transform-filter";
+
 import user from "./user_reducer";
 import store from "./store_reducer";
 import product from "./product_reducer";
+
+const authSubsetFilter = createFilter("user", ["userData"]);
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+  transforms: [authSubsetFilter]
+};
 
 const rootReducer = combineReducers({
   user,
   store,
   product
-})
+});
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
--- client/src/index.js
+++ client/src/index.js
@@ -5,22 +5,26 @@ import App from './App';
 //redux
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+const store = createStoreWithMiddleware(Reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__()  
+);
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider
-      store={createStoreWithMiddleware(Reducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()  
-      )}
-    >
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
